bd782ff00e95f525e4d05e023f98272df58884ba
Maheen Jan <maheenali0101@gmail.com>
2026-06-19 16:49:24 +0530

    Create 06_fleet_preference_query.sql

---
+++ sql/06_fleet_preference_query.sql
@@ -0,0 +1,10 @@
+-- Executed query to analyze the interaction between user type, rideable type, volume, and duration: 
+SQL 
+SELECT 
+member_casual, 
+rideable_type, 
+COUNT(*) AS total_trips, 
+ROUND(AVG(TIMESTAMP_DIFF(ended_at, started_at, MINUTE)), 2) AS avg_duration_mins 
+FROM `nifty-motif-496213-s3.cyclistic_tripdata.clean_trips_final` 
+GROUP BY member_casual, rideable_type 
+ORDER BY member_casual, total_trips DESC; 
