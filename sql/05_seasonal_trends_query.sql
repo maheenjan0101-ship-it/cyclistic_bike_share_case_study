017409eb4e7a077ff61f0bede7328c946c2723e5
Maheen Jan <maheenali0101@gmail.com>
2026-06-19 16:48:19 +0530

    Create 05_seasonal_trends_query.sql

---
+++ sql/05_seasonal_trends_query.sql
@@ -0,0 +1,10 @@
+-- Executed query to track monthly volume fluctuations and duration elasticity across the 12-month timeline: 
+SQL 
+SELECT  
+member_casual,  
+month_name,  
+COUNT(*) AS total_trips,  
+ROUND(AVG(ride_length), 2) AS average_ride_length_minutes  
+FROM `nifty-motif-496213-s3.cyclistic_tripdata.clean_trips_final`  
+GROUP BY member_casual, month_name  
+ORDER BY member_casual, total_trips DESC;
